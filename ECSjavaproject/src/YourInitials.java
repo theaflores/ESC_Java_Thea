fbc2591ee952562ca276dd1aa63cacbd0345fb45
theaflores <FloresF5866@sduhsd.lan>
2017-11-30 10:14:05 -0800

    Initials Project

---
+++ ECSjavaproject/src/YourInitials.java
@@ -0,0 +1,20 @@
+
+public class YourInitials 
+{
+	
+
+	public static void main(String[] args)
+	{
+		System.out.println("    FFFFFFFFFFF   M                   M    FFFFFFFFFFF");
+		System.out.println("    F             M M               M M    F");
+		System.out.println("    F             M  M             M  M    F");
+		System.out.println("    F             M   M           M   M    F");
+		System.out.println("    FFFFFF        M    M         M    M    FFFFF");
+		System.out.println("    F             M     M       M     M    F");
+		System.out.println("    F             M      M     M      M    F");
+		System.out.println("    F             M       M   M       M    F");
+		System.out.println("    F             M         M         M    F");  
+		
+	}
+	
+}
