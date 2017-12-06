698762dfc05671d81c65504cb1bff05a5c2dd2dc
theaflores <FloresF5866@sduhsd.lan>
2017-12-06 12:24:57 -0800

    two questions
    keyboard close

--- Selection Structures/src/BMICalcbetter.java
+++ Selection Structures/src/BMICalcbetter.java
@@ -1,4 +1,50 @@
+import java.util.Scanner;
 
-public class BMICalcbetter {
+public class BMICalcbetter 
+{
+	public static void main(String[] args)
+	{
+		Scanner keyboard = new Scanner(System.in);
+		
+		double heightfm,heightf,weightp,weightk,bmi,heighti,heightim,heightm;
+	
+		System.out.println("BMI (Body Mass Index) is a way of representing your size!");
+		
+		System.out.println("How many feet are you? ");
+		heightf = keyboard.nextDouble();
+		
+		System.out.println("How many inches? ");
+		heighti = keyboard.nextDouble();
+
+		System.out.println("How many pounds do you weigh? ");
+		weightp = keyboard.nextDouble();
+
+		weightk = weightp * .4536;
+		heightfm = heightf * .3048;
+		heightim = heighti * .0254;
+		
+		heightm = heightfm + heightim;
+		
+		bmi =( weightk /( heightm * heightm));
+		System.out.println("Your BMI is" + bmi + ".");
+	
+		if (bmi<18.5)
+		{
+			System.out.println("You're underweight.");
+		}
+		if ((bmi<24.9) && (bmi>18.5))
+		{
+			System.out.println("You're normal weight.");
+		}
+		if ((bmi<29.9) && (bmi>25.0))
+		{
+			System.out.println("You're overweight.");
+		}
+		if (bmi>30.0)
+		{
+			System.out.println("You're obese :)");
+		}
+	keyboard.close();
+	}
 
 }
